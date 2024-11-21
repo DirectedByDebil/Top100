e066491d9321669eef9980b75eb48fc8b5a46b52
xannykov <sema_zemlyakov@mail.ru>
2024-11-21 23:27:38 +0300

    feat(logic): add dark/light theme change via system settings feat(ui): add files for light/dark theme fix(ui): fixing the Shirt to be all over the Frame delete(ui): deleted unnecessary icons/backgrounds

--- Top100/Top100/Platforms/Android/MainActivity.cs
+++ Top100/Top100/Platforms/Android/MainActivity.cs
@@ -1,11 +1,44 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Content.Res;
 using Android.OS;
+using Android.Views;
+using Microsoft.Maui;
 
 namespace Top100
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | 
+        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            UpdateStatusBarColor();
+        }
+
+        private void UpdateStatusBarColor()
+        {
+            var isDarkTheme = (Resources.Configuration.UiMode & UiMode.NightMask) == UiMode.NightYes;
+
+            if (isDarkTheme)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#2E2C38"));
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.Visible;
+            }
+            else
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#FFFFFF"));
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)SystemUiFlags.LightStatusBar;
+            }
+        }
+
+        public override void OnConfigurationChanged(Android.Content.Res.Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            UpdateStatusBarColor();
+        }
     }
 }
